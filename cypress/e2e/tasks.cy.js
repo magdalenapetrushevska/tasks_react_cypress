f976fb4d3c1820fbec3105aa3548dfe460f37c4d
magdalenapetrushevska <magdalenapetrusevska9@gmail.com>
2025-03-12 17:55:58 +0100

    add baseUrl

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,35 +1,33 @@
-/// <reference types="Cypress"/>
-
 describe("tasks page", () => {
   it("should display the title", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get(".main-header").find("h1").contains("Today's Task");
   });
 
   it("should display the main image", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get(".main-header").find("img");
   });
 
   it("should display information for empty task list", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get("main").find('[data-cy="no-tasks"]');
   });
 
   it("should display filter options", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get("#filter").find("option").should("have.length", 5);
   });
 
   it("should display button for adding task", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get('[data-cy="start-add-task-button"]').contains("Add Task");
   });
 });
 
 describe("add task modal", () => {
   it("should contains inputs fields in the modal", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get('[data-cy="start-add-task-button"]').click();
     cy.get('[data-cy="title-input"]');
     cy.get('[data-cy="summary-input"]');
@@ -41,7 +39,7 @@ describe("add task modal", () => {
 
 describe("tasks interactions", () => {
   it("should open and close the new task modal", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get('[data-cy="start-add-task-button"]').click();
     cy.get('[data-cy="modal"]').should("exist");
     cy.get('[data-cy="cancel"]').click();
@@ -49,7 +47,7 @@ describe("tasks interactions", () => {
   });
 
   it("should add new task", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get('[data-cy="start-add-task-button"]').click();
     cy.get('[data-cy="title-input"]').type("Task 1");
     cy.get('[data-cy="summary-input"]').type("Description for task 1");
@@ -60,7 +58,7 @@ describe("tasks interactions", () => {
   });
 
   it("should add multiple tasks", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
 
     cy.get('[data-cy="start-add-task-button"]').click();
     cy.get('[data-cy="title-input"]').type("Task 1");
@@ -82,14 +80,14 @@ describe("tasks interactions", () => {
   });
 
   it("should validate user input", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get('[data-cy="start-add-task-button"]').click();
     cy.get('[data-cy="submit"]').click();
     cy.get('[data-cy="error-message"]').contains("Please provide values");
   });
 
   it("should filter tasks", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("/");
     cy.get('[data-cy="start-add-task-button"]').click();
     cy.get('[data-cy="title-input"]').type("Task 1");
     cy.get('[data-cy="summary-input"]').type("Description for task 1");
